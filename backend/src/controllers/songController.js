fe7abd58df33a7931c929c890eb9d86eab451204
Shravan Sapkota <sapkota.shravan@gmail.com>
2026-07-21 18:19:02 +0545

    add test function

--- backend/src/controllers/songController.js
+++ backend/src/controllers/songController.js
@@ -1,8 +1,21 @@
+import pool  from "../config/db";
+
 const getAllSongs=(req,res)=>{
+    try{
+    const [rows]=await pool.query("SELECT * FROM songs");
     res.status(200).json({
         success:true,
-        message:"get all songs controller working"
+        message:"get all songs controller working",
+        songs:rows
     });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:"server error"
+        })
+        console.log(error.message);
+    }
 }
 
 export {getAllSongs};
