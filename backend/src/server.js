c75558c0b4b4bc0d70fda975830844ec1a492135
Shravan Sapkota <sapkota.shravan@gmail.com>
2026-07-21 11:17:03 +0545

    add server configuration

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config()
+
+const PORT= process.env.PORT || 5000;
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
