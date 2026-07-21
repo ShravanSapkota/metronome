85e492dacf8543c9cd01a22ba7b12e943b65f1ca
Shravan Sapkota <sapkota.shravan@gmail.com>
2026-07-21 15:12:56 +0545

    add db connect

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import pool from "./config/db.js";
 
 dotenv.config()
 
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-});
+const startserver = async () => {
+    try {
+        const connection = await pool.getConnection();
+        console.log("Connected to mysql database");
+        connection.release();
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    }
+    catch (error) {
+        console.log("connection failed",error.message)
+    }
+}
+
+startserver();
