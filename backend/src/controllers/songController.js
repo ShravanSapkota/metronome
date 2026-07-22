d50939c2e5433130625601852e0e3d6aaf845821
Shravan Sapkota <sapkota.shravan@gmail.com>
2026-07-22 13:25:08 +0545

    add filter by bpm on search

--- backend/src/controllers/songController.js
+++ backend/src/controllers/songController.js
@@ -20,35 +20,54 @@ const getAllSongs = async (req, res) => {
 
 const searchSong = async (req, res) => {
     try {
-        const {query} = req.query
-        if (!query || query == "") {
-            return res.status(400).json({
+        const { query, minbpm, maxbpm } = req.query
+
+        let sql = "SELECT * FROM songs WHERE 1=1 "
+        let values = []
+
+        if (!query && !(minbpm && maxbpm)) {
+            return res.status(500).json({
                 success: false,
-                message: "provide a valid search input"
+                message: "please enter a valid search input or bpm range"
             })
         }
-        const search = `%${query.trim()}%`
+        if (query) {
+            const trimmedQuery = query.trim()
+            sql += "AND (song like ? OR artist like ?) "
+            values.push(`%${trimmedQuery}%`, `%${trimmedQuery}%`)
+        }
+        if (minbpm && maxbpm) {
+            const min = Number(minbpm);
+            const max = Number(maxbpm);
 
-        const [rows] = await pool.query(`SELECT  * FROM songs 
-            WHERE artist like ?
-            OR song like ?
-            ORDER BY popularity DESC
-            LIMIT 20`,[search,search])
+            if (Number.isNaN(min) || Number.isNaN(max)) {
+                return res.status(400).json({
+                    success: false,
+                    message: "BPM values must be valid numbers."
+                });
+            }
+            sql += "AND (tempo between ? AND ?) "
+            values.push(min, max)
+        }
 
-        res.status(200).json({
-            success:true,
-            message:"search results successfull",
-            songs:rows
+        sql += "ORDER BY popularity DESC"
+
+        const [rows] = await pool.query(sql, values);
+
+        res.json({
+            success: true,
+            songs: rows,
+            count: rows.length
         })
 
     }
-    catch (error) { 
+    catch (error) {
         console.log(error.message)
         res.status(500).json({
-            success:false,
-            message:"Internal server error"
+            success: false,
+            message: "Internal server error"
         })
     }
 }
 
-export { getAllSongs,searchSong };
+export { getAllSongs, searchSong };
